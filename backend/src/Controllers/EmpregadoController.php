a61aba4ca46e769c2f9de7e536b4af4c3e2c06a0
Istarie <zIstarie@proton.me>
2023-05-01 03:24:00 -0300

    feat: Empregado Destroy Method on API Controller

--- backend/src/Controllers/EmpregadoController.php
+++ backend/src/Controllers/EmpregadoController.php
@@ -3,6 +3,7 @@
 namespace Portfolio\Src\Controllers;
 
 use Doctrine\ORM\EntityManager;
+use Exception;
 use Portfolio\Config\EntityManager as ConfigEntityManager;
 use Portfolio\Infra\Entities\Empregado;
 use Portfolio\Src\Strategies\ApiController;
@@ -22,7 +23,11 @@ class EmpregadoController implements ApiController
             ->getRepository(Empregado::class)
             ->findAll();
 
-        return $empregados;
+        http_response_code(200);
+        return json_encode([
+            'data' => $empregados,
+            'status' => http_response_code()
+        ]);
     }
 
     public function store(array|object $data): void
@@ -60,7 +65,30 @@ class EmpregadoController implements ApiController
 
     public function destroy(int $id)
     {
+        try {
+            $empregado = $this->entityManager->find(Empregado::class, $id);
+            if ($empregado) {
+                $this->entityManager->remove($empregado);
+                $this->entityManager->flush();
+            }
 
+            http_response_code(200);
+            return json_encode([
+                'message' => 'Entity ' . $id . ' successfully removed from the database',
+                'status' => http_response_code()
+            ]);
+        } catch (Exception $e) {
+            $logFile = fopen('../../logs/' . date('Y-m-d H:i:s') . '-log.txt', 'w');
+            $content = "Erro ao persistir remoção de entidade 'Empregado' de id: '$id' na base de dados. Mensagem de erro: {$e->getMessage()}";
+            fwrite($logFile, $content);
+            fclose($logFile);
+
+            http_response_code(500);
+            return json_encode([
+                'message' => 'Failed trying to remove entity from the database',
+                'status' => http_response_code()
+            ]);
+        }
     }
 }
 
